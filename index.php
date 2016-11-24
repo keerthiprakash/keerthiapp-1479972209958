03ea19755f12c164859e1f2055c5f28b9a92aad2
keerthi prakash thirugnanam <keerthi.t@infosys.com>
2016-11-24 01:25:05 -0600

    commit by keerthi

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+				<h1 id = "message"><?php echo "Hello MY World Done by keerthi"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
